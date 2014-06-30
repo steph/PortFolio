6083164720c8a85d3c82a10f7b754c5ad86d0582
stephane <steph>
2014-06-30 07:49:51 +0200

    suppression Brant Nav

---
+++ src/application/controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php 
+/**
+ * Controller par défaut de l'application, correspond à la page d'index du site
+ * 
+ * @category Application
+ * @author Formateur
+ * @version 0.0.1
+ */
+class TestController extends Controller
+{    
+    public function action()
+    {
+
+    }
+}
